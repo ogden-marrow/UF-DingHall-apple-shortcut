10d936674591426bb08ac9be946f112e3dcb9fff
John E. Palenchar <jpalenchar.3@gmail.com>
2021-03-21 21:08:45 -0400

    fix Stuff

--- GetASPX.sh
+++ GetASPX.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 curl https://www.bsd.ufl.edu/dining/Hours/RegularHours.aspx >> hoursn.txt
-H = './hours.txt'
-N = './hoursn.txt'
+H='./hours.txt'
+N='./hoursn.txt'
 
-Cc = shasum -a 512 H
-Nn = shasum -a 512 N
-if [ $Cc == $Nn]
+Cc=$(shasum -a 512 $H | rev)
+Nn=$(shasum -a 512 $N | rev)
+
+echo $Cc
+echo $Nn
+if [ $Cc -eq $Nn ]
 then
-    rm -f N
+    rm -f $N
 fi
